7a559640f7f472618da02680229798714e0fca22
Johannes Teklote <johannes.teklote@web.de>
2018-03-22 00:47:22 +0100

    update to wroking ldap

--- src/main/java/de/figge/cocktailabend/config/SpringSecurityConfig.java
+++ src/main/java/de/figge/cocktailabend/config/SpringSecurityConfig.java
@@ -1,25 +1,35 @@
 package de.figge.cocktailabend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
+@PropertySource("classpath:config.properties")
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AccessDeniedHandler accessDeniedHandler;
+    //TODO create file config.properties with properties admin.usernam and admin.password and fill it with the credentials
+    @Value("${admin.dn}")
+    private String adminDn;
+    @Value("${admin.password}")
+    private String adminPassword;
+    @Value("${ldap.url}")
+    private String ldapUrl;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/admin/**", "/console/**", "/statistics/**", "/authenticate").hasAnyRole("ADMIN")
@@ -34,10 +44,27 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
-        //TODO update ldap url and password attribute -> nez-ag
-        auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource().url("ldap://localhost:8389/dc=springframework,dc=org")
-                .and().passwordCompare().passwordEncoder(new LdapShaPasswordEncoder()).passwordAttribute("userPassword");
+        auth.ldapAuthentication()
+                .userDnPatterns("cn={0},ou=people").groupSearchBase("ou=groups").groupRoleAttribute("member").contextSource()
+                .managerDn(adminDn).managerPassword(adminPassword)
+                .url(ldapUrl)
+                .and().passwordCompare().passwordAttribute("userPassword")
+                .and().passwordEncoder(passwordEncoder());
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        final LdapShaPasswordEncoder sha = new LdapShaPasswordEncoder();
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return sha.encodePassword(rawPassword.toString(), null);
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return sha.isPasswordValid(encodedPassword, rawPassword.toString(), null);
+            }
+        };
     }
 
     @Override
